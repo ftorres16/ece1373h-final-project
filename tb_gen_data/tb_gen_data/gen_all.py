42ee525a41f598856753319e106b4cc175e1809e
Fabian Torres <36959980+ftorres16@users.noreply.github>
2021-04-27 01:21:43 -0300

    gen batch norm 2d data

--- tb_gen_data/tb_gen_data/gen_all.py
+++ tb_gen_data/tb_gen_data/gen_all.py
@@ -1,12 +1,19 @@
 import torch
 
+from tb_gen_data.gen_batch_norm import GenBatchNorm2D
 from tb_gen_data.gen_cnn import GenCNN
 from tb_gen_data.gen_fc import GenFC
 from tb_gen_data.gen_max_pool_2d import GenMaxPool2D
 from tb_gen_data.gen_relu import GenReLU
 
 if __name__ == "__main__":
-    gens = {GenCNN: "cnn", GenFC: "fc", GenMaxPool2D: "max_pool_2d", GenReLU: "relu"}
+    gens = {
+        GenBatchNorm2D: "batch_norm_2d",
+        GenCNN: "cnn",
+        GenFC: "fc",
+        GenMaxPool2D: "max_pool_2d",
+        GenReLU: "relu",
+    }
 
     for gen_, name in gens.items():
         torch.manual_seed(0)
