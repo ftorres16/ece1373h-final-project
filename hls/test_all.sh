43800758f20944d507f72204caa43f1e9865c206
Fabian Torres <36959980+ftorres16@users.noreply.github>
2021-04-28 18:31:32 -0300

    script for testing all nn layers

---
+++ hls/test_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run inside the hls folder
+
+echo "Compiling..."
+
+make all
+
+echo ""
+echo "Testing..."
+
+./bin/batch_norm_2d
+./bin/cnn
+./bin/fc
+./bin/max_pool_2d
+./bin/relu
