fc2be06c29f82b54e845b2e84d823d96c3127db5
Fabian Torres <36959980+ftorres16@users.noreply.github>
2021-07-19 16:30:23 -0400

    hls code for full nn with memory reuse

--- hls/src/full_nn.cpp
+++ hls/src/full_nn.cpp
@@ -4,16 +4,10 @@
 #include "relu.h"
 
 void full_nn(float *mem, const int params_offset, const int input_offset,
-             const int intermediate_results_offset, const int output_offset,
-             const int b, const int ix, const int iy) {
+             const int output_offset, const int b, const int ix, const int iy) {
 
   int params_offset_0, params_offset_1, params_offset_2, params_offset_3,
       params_offset_4, params_offset_5, params_offset_fc_1, params_offset_fc_2;
-
-  int output_offset_0 = 0, output_offset_1 = 0, y0_offset_1 = 0,
-      output_offset_2 = 0, y0_offset_2 = 0, output_offset_3 = 0,
-      y0_offset_3 = 0, output_offset_4 = 0, y0_offset_4 = 0,
-      output_offset_5 = 0, y0_offset_5 = 0, output_offset_fc_1 = 0;
 
   CONV_LAYER_PARAMS conv_stack_0, conv_stack_1, conv_stack_2, conv_stack_3,
       conv_stack_4, conv_stack_5, conv_fc_1, conv_fc_2;
@@ -165,56 +159,29 @@ void full_nn(float *mem, const int params_offset, const int input_offset,
   params_offset_fc_2 =
       params_offset_fc_1 + get_conv_num_params(conv_fc_1) * sizeof(float);
 
-  output_offset_0 = intermediate_results_offset;
-  y0_offset_1 =
-      output_offset_0 + get_conv_num_outputs(conv_stack_0) * sizeof(float);
-  output_offset_1 =
-      y0_offset_1 + get_conv_num_outputs(conv_stack_1) * sizeof(float);
-  y0_offset_2 = output_offset_1 +
-                get_max_pool_2d_num_outputs(max_pool_stack_1) * sizeof(float);
-  output_offset_2 =
-      y0_offset_2 + get_conv_num_outputs(conv_stack_2) * sizeof(float);
-  y0_offset_3 = output_offset_2 +
-                get_max_pool_2d_num_outputs(max_pool_stack_2) * sizeof(float);
-  output_offset_3 =
-      y0_offset_3 + get_conv_num_outputs(conv_stack_3) * sizeof(float);
-  y0_offset_4 = output_offset_3 +
-                get_max_pool_2d_num_outputs(max_pool_stack_3) * sizeof(float);
-  output_offset_4 =
-      y0_offset_4 + get_conv_num_outputs(conv_stack_4) * sizeof(float);
-  y0_offset_5 = output_offset_4 +
-                get_max_pool_2d_num_outputs(max_pool_stack_4) * sizeof(float);
-  output_offset_5 =
-      y0_offset_5 + get_conv_num_outputs(conv_stack_5) * sizeof(float);
-  output_offset_fc_1 =
-      output_offset_5 +
-      get_max_pool_2d_num_outputs(max_pool_stack_5) * sizeof(float);
-
   // Neural network computation
-  conv_batch_relu_layer(mem, params_offset_0, input_offset, output_offset_0,
+  conv_batch_relu_layer(mem, params_offset_0, input_offset, output_offset,
                         conv_stack_0);
 
-  conv_batch_relu_max_layer(mem, params_offset_1, output_offset_0, y0_offset_1,
-                            output_offset_1, conv_stack_1, max_pool_stack_1);
+  conv_batch_relu_max_layer(mem, params_offset_1, output_offset, input_offset,
+                            conv_stack_1, max_pool_stack_1);
 
-  conv_batch_relu_max_layer(mem, params_offset_2, output_offset_1, y0_offset_2,
-                            output_offset_2, conv_stack_2, max_pool_stack_2);
+  conv_batch_relu_max_layer(mem, params_offset_2, output_offset, input_offset,
+                            conv_stack_2, max_pool_stack_2);
 
-  conv_batch_relu_max_layer(mem, params_offset_3, output_offset_2, y0_offset_3,
-                            output_offset_3, conv_stack_3, max_pool_stack_3);
+  conv_batch_relu_max_layer(mem, params_offset_3, output_offset, input_offset,
+                            conv_stack_3, max_pool_stack_3);
 
-  conv_batch_relu_max_layer(mem, params_offset_4, output_offset_3, y0_offset_4,
-                            output_offset_4, conv_stack_4, max_pool_stack_4);
+  conv_batch_relu_max_layer(mem, params_offset_4, output_offset, input_offset,
+                            conv_stack_4, max_pool_stack_4);
 
-  conv_batch_relu_max_layer(mem, params_offset_5, output_offset_4, y0_offset_5,
-                            output_offset_5, conv_stack_5, max_pool_stack_5);
+  conv_batch_relu_max_layer(mem, params_offset_5, output_offset, input_offset,
+                            conv_stack_5, max_pool_stack_5);
 
-  conv_layer(mem, params_offset_fc_1, output_offset_5, output_offset_fc_1,
-             conv_fc_1);
+  conv_layer(mem, params_offset_fc_1, output_offset, input_offset, conv_fc_1);
 
-  relu_layer(mem, output_offset_fc_1, output_offset_fc_1,
+  relu_layer(mem, input_offset, input_offset,
              conv_fc_1.b * conv_fc_1.od * conv_fc_1.ox * conv_fc_1.oy);
 
-  conv_layer(mem, params_offset_fc_2, output_offset_fc_1, output_offset,
-             conv_fc_2);
+  conv_layer(mem, params_offset_fc_2, input_offset, output_offset, conv_fc_2);
 }
--- hls/src/full_nn.h
+++ hls/src/full_nn.h
@@ -2,7 +2,6 @@
 #define FULL_NN_H
 
 void full_nn(float *mem, const int params_offset, const int input_offset,
-             const int intermediate_results_offset, const int output_offset,
-             const int b, const int ix, const int iy);
+             const int output_offset, const int b, const int ix, const int iy);
 
 #endif
