7774a3a73405fca35bc21ba88af764781bd244f5
Fabian Torres <36959980+ftorres16@users.noreply.github>
2021-08-30 20:46:41 -0400

    fix spike deepclassifier directives

--- hls_proj/spike_deepclassifier/solution1/directives.tcl
+++ hls_proj/spike_deepclassifier/solution1/directives.tcl
@@ -5,12 +5,7 @@
 ############################################################
 set_directive_interface -mode s_axilite -bundle CTRL_BUS "spike_deepclassifier"
 set_directive_interface -mode m_axi -depth 477587 "spike_deepclassifier" mem
-set_directive_interface -mode s_axilite -bundle CTRL_BUS "spike_deepclassifier" mem_params
+set_directive_interface -mode s_axilite -bundle CTRL_BUS "spike_deepclassifier" depetector_mem_params
+set_directive_interface -mode s_axilite -bundle CTRL_BUS "spike_deepclassifier" bar_mem_params
 set_directive_interface -mode s_axilite -bundle CTRL_BUS "spike_deepclassifier" n_electrodes
 set_directive_interface -mode m_axi -depth 6 "spike_deepclassifier" electrodes_offset
-# set_directive_interface -mode s_axilite -bundle CTRL_BUS "spike_deepclassifier"
-# set_directive_interface -mode m_axi -depth 2424292 "spike_deepclassifier" mem
-# set_directive_interface -mode s_axilite -bundle CTRL_BUS "spike_deepclassifier" deeptector_mem_params
-# set_directive_interface -mode s_axilite -bundle CTRL_BUS "spike_deepclassifier" bar_mem_params
-# set_directive_interface -mode m_axi -depth 5 "spike_deepclassifier" electrodes_offset
-# set_directive_interface -mode s_axilite -bundle CTRL_BUS "spike_deepclassifier" n_electrodes
